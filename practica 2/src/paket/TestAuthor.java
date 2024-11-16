c8f05176ac1addeb7bce38a9707fae4b3c6d6c4c
NikolaiZabudko <uncofffi@gmail.com>
2024-11-16 14:27:25 +0300

    Add files via upload

---
+++ practica 2/src/paket/TestAuthor.java
@@ -0,0 +1,8 @@
+package paket;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author a1 = new Author("Aleksandr", "Aleksandrauthor@mail.ru", 'M');
+        System.out.println(a1);
+    }
+}
